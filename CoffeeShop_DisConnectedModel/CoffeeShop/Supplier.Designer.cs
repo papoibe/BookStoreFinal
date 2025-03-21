b2d4f16cd76587e8c7e59a2894d43fb2d64d6491
Phuoc Thinh <113354085+papoibe@users.noreply.github.com>
2025-03-21 16:38:34 +0700

    Add files via upload
    upload C# dis

---
+++ CoffeeShop_DisConnectedModel/CoffeeShop/Supplier.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace CoffeeShop
+{
+    partial class Supplier
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGVSql = new System.Windows.Forms.DataGridView();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.ma = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ten = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGVSql)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGVSql
+            // 
+            this.dataGVSql.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGVSql.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ma,
+            this.ten});
+            this.dataGVSql.Location = new System.Drawing.Point(-2, 0);
+            this.dataGVSql.Name = "dataGVSql";
+            this.dataGVSql.Size = new System.Drawing.Size(802, 270);
+            this.dataGVSql.TabIndex = 0;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(-2, 276);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(109, 43);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // ma
+            // 
+            this.ma.DataPropertyName = "id";
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.Red;
+            this.ma.DefaultCellStyle = dataGridViewCellStyle3;
+            this.ma.HeaderText = "Mã SP";
+            this.ma.Name = "ma";
+            // 
+            // ten
+            // 
+            this.ten.DataPropertyName = "Name";
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.Red;
+            this.ten.DefaultCellStyle = dataGridViewCellStyle4;
+            this.ten.HeaderText = "Tên SP";
+            this.ten.Name = "ten";
+            // 
+            // Supplier
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.dataGVSql);
+            this.Name = "Supplier";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Supplier_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGVSql)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGVSql;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ma;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ten;
+    }
+}
+
---
+++ CoffeeShop_DisConnectedModel/CoffeeShop/Supplier.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+namespace CoffeeShop
+{
+    public partial class Supplier : Form
+    {
+        /*
+        // Disconnect Model
+        // sqlDataAdapter adapter 
+        adapter.Fill()
+        apdater.Update()
+        //Dataset (DataTable)
+
+        */
+        private SqlDataAdapter adapter;
+        private DataSet dataSet;
+        private SqlConnection cn; 
+        public Supplier()
+        {
+            InitializeComponent();
+            string cnStr = "Data Source=.;Initial Catalog=CoffeeShop;Integrated Security=True";
+            cn = new SqlConnection(cnStr);
+        }
+
+        private void Supplier_Load(object sender, EventArgs e)
+        {
+            dataSet = new DataSet();
+            string sql = "SELECT * FROM Product";
+            adapter = new SqlDataAdapter(sql, cn);
+            adapter.Fill(dataSet);
+       
+            dataGVSql.DataSource = dataSet.Tables[0]; // xu ly de hien thi
+        }
+
+        private void btnSave_Click(object sender, EventArgs e) // nút save khi chỉnh sửa trên bảng
+        {
+
+
+            try
+            {
+                //delete
+                string sql = "DELETE FROM Product WHERE id = @ma";
+                SqlCommand cmd = new SqlCommand(sql, cn);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.Add("@ma", SqlDbType.VarChar, 10, "id");
+                adapter.DeleteCommand = cmd;
+
+                //adapter.Update(dataSet);
+
+                //INSERT
+                String insertSql = "INSERT INTO Product (id, name, purchasePrice, sellingPrice, categoryId, supplierId) VALUES (@id, @name, @purchasePrice, @sellingPrice, @categoryId, @supplierId)";
+                SqlCommand insertCmd = new SqlCommand(insertSql, cn);
+                insertCmd.CommandType = CommandType.Text;
+                insertCmd.Parameters.Add("@id", SqlDbType.VarChar, 10, "id");
+                insertCmd.Parameters.Add("@name", SqlDbType.NVarChar, 100, "name");
+                insertCmd.Parameters.Add("@purchasePrice", SqlDbType.Float, 0, "purchasePrice");
+                insertCmd.Parameters.Add("@sellingPrice", SqlDbType.Float, 0, "sellingPrice");
+                insertCmd.Parameters.Add("@categoryId", SqlDbType.Int, 0, "categoryId");
+                insertCmd.Parameters.Add("@supplierId", SqlDbType.VarChar, 20, "supplierId");
+
+                adapter.InsertCommand = insertCmd;
+                //adapter.Update(dataSet);
+
+
+                //UPDATE
+                String updateSql = "UPDATE Product SET name = @name, purchasePrice = @purchasePrice, sellingPrice = @sellingPrice, categoryId = @categoryId, supplierId = @supplierId WHERE id = @id";
+                SqlCommand updateCmd = new SqlCommand(updateSql, cn);
+                updateCmd.CommandType = CommandType.Text;
+                updateCmd.Parameters.Add("@id", SqlDbType.VarChar, 10, "id");
+                updateCmd.Parameters.Add("@name", SqlDbType.NVarChar, 100, "name");
+                updateCmd.Parameters.Add("@purchasePrice", SqlDbType.Float, 0, "purchasePrice");
+                updateCmd.Parameters.Add("@sellingPrice", SqlDbType.Float, 0, "sellingPrice");
+                updateCmd.Parameters.Add("@categoryId", SqlDbType.Int, 0, "categoryId");
+                updateCmd.Parameters.Add("@supplierId", SqlDbType.VarChar, 20, "supplierId");
+
+                adapter.UpdateCommand = updateCmd;
+                adapter.Update(dataSet);
+
+
+
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message, "lỗi rồi nè mẹ");
+            }
+
+        }
+    }
+}
